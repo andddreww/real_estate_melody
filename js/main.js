499ac55446d830e2f0de8c3647ea264f666b3e96
andddreww <112699418+andddreww@users.noreply.github.com>
2022-10-10 22:58:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    var currentFloor = 2; // переменная, где хранится текущий этаж
+    var floorPath =  $('.floors path'); // каждый отдельный этаж в SVG
+    var counterUp = $('.counter-up'); /* кнопка увеличения этажа */
+    var counterDown = $('.counter-down'); /* кнопка уменьшения этажа */
+    var modal = $('.modal');
+    var modalCloseButton = $('.modal-close-button');
+    var viewFlatsButton = $('.view-flats');
+
+    // функция при наведении мышкой на этаж
+    floorPath.on('mouseover', function() {
+    floorPath.removeClass('current-floor'); // удаляем активный класс у этажей
+    currentFloor = $(this).attr('data-floor'); // получаем значение текущего этажа
+    $('.counter').text(currentFloor); // записываем значение этажа в счетчик справа
+ });
+
+    floorPath.on('click', toggleModal); // при клике на этаж, вызвать окно
+    modalCloseButton.on('click', toggleModal); // при клике на кнопку Закрыть - закыть окно
+    viewFlatsButton.on('click', toggleModal);
+ // отслеживаем клик по кнопке вверх
+ counterUp.on('click', function() { 
+    // проверяем знаяение этажа, не должно быть > 18
+    if (currentFloor < 18) { 
+        currentFloor++; // прибавляем один этаж
+        usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGroupping: false }); //форматируем переменную с этажом, чтобы было 02, а не 2
+        $('.counter').text(usCurrentFloor); // записываем знасение этажа в счетчик справа
+        floorPath.removeClass('current-floor'); // удаляем активный класс у этажей
+        $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor'); // подсвечиваем текущий этаж
+    }
+ });
+
+ counterDown.on('click', function() {
+    if (currentFloor > 2) {
+        currentFloor--;
+        usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGroupping: false });
+        $('.counter').text(usCurrentFloor);
+        floorPath.removeClass('current-floor');
+        $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+    }
+
+ });
+ function toggleModal() { 
+    // функция открыть-закрыть окно
+    modal.toggleClass('is-open');
+}
+});
